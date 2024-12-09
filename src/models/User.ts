9ea204889f69bcdc7383dd5ea9f1c06440628ffe
ErickGBR <erickburgos1519@gmail.com>
2024-12-09 00:42:08 -0600

    feat: user schema mongo

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+})
+
+const user = mongoose.model("user", userSchema)
+export default user;
