94d47ab1b6a1dc47d1dbe4f0a25738e143c8ceab
ErickGBR <erickburgos1519@gmail.com>
2024-12-20 22:37:39 -0600

    feat: add auhentication util to register user

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
 import User from "../models/User";
-
+import { hashPassword } from "../utils/auth";
 export const createAccount = async (req: Request, res: Response) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
     const userExist = await User.findOne({ email });
 
     if(userExist) {
@@ -12,6 +12,7 @@ export const createAccount = async (req: Request, res: Response) => {
         })
     }else {
         const user = new User(req.body);
+        user.password = await hashPassword(password);
         await user.save();
         res.status(200).json(user);
     }
