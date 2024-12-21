da9b1d188fc7ee68ab66f397505802c16ee7c345
ErickGBR <erickburgos1519@gmail.com>
2024-12-20 22:13:26 -0600

    fix: show message error

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,20 +2,20 @@ import { Request, Response } from "express";
 import User from "../models/User";
 
 export const createAccount = async (req: Request, res: Response) => {
-    const { email } = req.query;
-    const userExist = await User.findOne({email});
+    const { email } = req.body;
+    const userExist = await User.findOne({ email });
 
     if(userExist) {
         const error = new Error("This user already exist!");
         res.status(409).json({
             message: error.message
         })
+    }else {
+        const user = new User(req.body);
+        await user.save();
+        res.status(200).json(user);
     }
 
-    const user = new User(req.body);
-    await user.save();
-
-    res.status(200).send(user);
 }
 
 
