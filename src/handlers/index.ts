5aefd47d88eeecc62d373ec10856f6743365a19b
ErickGBR <erickburgos1519@gmail.com>
2024-12-20 21:25:11 -0600

    feat: add handler user creation

---
+++ src/handlers/index.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import User from "../models/User";
+
+export const createAccount = async (req: Request, res: Response) => {
+    const { email } = req.query;
+    const userExist = await User.findOne({email});
+
+    if(userExist) {
+        const error = new Error("This user already exist!");
+        res.status(409).json({
+            message: error.message
+        })
+    }
+
+    const user = new User(req.body);
+    await user.save();
+
+    res.status(200).send(user);
+}
+
+
