ff6cc56f5d003407466a97d0e582b4657008a5d0
ErickGBR <erickburgos1519@gmail.com>
2024-12-22 01:40:49 -0600

    feat: add handler user

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,20 +2,32 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 export const createAccount = async (req: Request, res: Response) => {
+    const slug = (await import('slug')).default;
     const { email, password } = req.body;
     const userExist = await User.findOne({ email });
 
-    if(userExist) {
+    if (userExist) {
         const error = new Error("This user already exist!");
         res.status(409).json({
             message: error.message
         })
-    }else {
-        const user = new User(req.body);
-        user.password = await hashPassword(password);
-        await user.save();
-        res.status(200).json(user);
     }
+
+    const handle = slug(req.body.handle, "");
+    const handleExist = await User.findOne({ handle });
+    if (handleExist) {
+        const error = new Error("This handle already exist!");
+        res.status(409).json({
+            message: error.message
+        })
+    }
+
+    const user = new User(req.body);
+    user.password = await hashPassword(password);
+    user.handle = handle;
+
+    await user.save();
+    res.status(200).json(user);
 
 }
 
