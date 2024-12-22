3a32fa9457c32698a27530440f1f89391ffea8d3
ErickGBR <erickburgos1519@gmail.com>
2024-12-22 02:06:47 -0600

    feat: add validations from the handle with express validations

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
-export const createAccount = async (req: Request, res: Response) => {
+
+export const createAccount = async (req: Request, res: Response): Promise<void>=> {
     const slug = (await import('slug')).default;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+    }
+
     const { email, password } = req.body;
     const userExist = await User.findOne({ email });
 
@@ -17,7 +25,7 @@ export const createAccount = async (req: Request, res: Response) => {
     const handleExist = await User.findOne({ handle });
     if (handleExist) {
         const error = new Error("This handle already exist!");
-        res.status(409).json({
+         res.status(409).json({
             message: error.message
         })
     }
@@ -27,7 +35,7 @@ export const createAccount = async (req: Request, res: Response) => {
     user.handle = handle;
 
     await user.save();
-    res.status(200).json(user);
+    res.status(201).json(user);
 
 }
 
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { createAccount } from "./handlers";
 
 const router = Router();
 
-router.post("/auth/register", createAccount);
+router.post("/auth/register",
+    body("handle").notEmpty(),
+     createAccount);
 
 export default router;
