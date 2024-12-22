679cfe28b9a78ae88fced64d9af882e44c6925f8
ErickGBR <erickburgos1519@gmail.com>
2024-12-22 02:29:05 -0600

    feat: add validations to all fields from the register

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,10 @@ import { createAccount } from "./handlers";
 const router = Router();
 
 router.post("/auth/register",
-    body("handle").notEmpty(),
+    body("handle").notEmpty().withMessage("Handle cannot be empty"),
+    body("name").notEmpty().withMessage("Name cannot be empty"),
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters long"),
      createAccount);
 
 export default router;
