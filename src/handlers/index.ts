bf388e78b307f67476a861172d792a8487aab0e2
ErickGBR <erickburgos1519@gmail.com>
2024-12-24 00:05:34 -0600

    feat: add login and validate passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import User from "../models/User";
-import { hashPassword } from "../utils/auth";
+import { hashPassword , checkPassword} from "../utils/auth";
 
-export const createAccount = async (req: Request, res: Response): Promise<void>=> {
+export const createAccount = async (req: Request, res: Response): Promise<void> => {
     const slug = (await import('slug')).default;
 
     const errors = validationResult(req);
@@ -25,7 +25,7 @@ export const createAccount = async (req: Request, res: Response): Promise<void>=
     const handleExist = await User.findOne({ handle });
     if (handleExist) {
         const error = new Error("This handle already exist!");
-         res.status(409).json({
+        res.status(409).json({
             message: error.message
         })
     }
@@ -39,4 +39,35 @@ export const createAccount = async (req: Request, res: Response): Promise<void>=
 
 }
 
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+
+    // Validate request body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    // Check if user exist
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error("User not found");
+        res.status(404).json({
+            message: error.message
+        })
+    }
+
+
+    const isPassword = await checkPassword(password, user.password)
+    // Check if password is correct
+    if (!isPassword) {
+        const error = new Error("Invalid password");
+        res.status(401).json({
+            message: error.message
+        })
+    }
+
+}
 
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body } from "express-validator";
-import { createAccount } from "./handlers";
+import { createAccount, login } from "./handlers";
 
 const router = Router();
 
@@ -9,6 +9,12 @@ router.post("/auth/register",
     body("name").notEmpty().withMessage("Name cannot be empty"),
     body("email").isEmail().withMessage("Email is not valid"),
     body("password").isLength({ min: 8 }).withMessage("Password must be at least 8 characters long"),
-     createAccount);
+    createAccount);
+
+
+router.post("/auth/login",
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password").notEmpty().withMessage("Password cannot be empty"),
+    login);
 
 export default router;
