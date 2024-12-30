1558303a25b505d05d2671ee0bdf515d14146bf2
ErickGBR <erickburgos1519@gmail.com>
2024-12-29 18:16:25 -0600

    feat: cors configuration

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,11 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
     origin: (origin, callback) => {
-        console.log(origin);
+        const whitelist = process.env.CORS_WHITELIST.split(",");
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
     }
 }
